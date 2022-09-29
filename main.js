84b9e71f1d546549ad574b5a5159b328bd0fac54
Anthony Mastores <mastores27@gmail.com>
2022-09-28 21:48:04 -0500

    vers 1 working

--- main.js
+++ main.js
@@ -1,66 +1,86 @@
-let timer 
-let deleteFirstPhotoDelay
+const next = document.querySelector(".next")
+const input = document.querySelector("input")
+const searchbutton = document.querySelector(".searchbutton")
 
-async function start() {
-    try {
-        const response = await fetch ("https://dog.ceo/api/breeds/list/all")
-        const data = await response.json()
-        createBreedList(data.message)
-    }catch(e) {
-        console.log("There was a problem fetching the breed list")
+let pagenr = 1
+let search = false
+let query = ""
+
+input.addEventListener("input",(e) => {
+    e.preventDefault()
+    query = e.target.value
+})
+
+async function CuratedPhotos(pagenr) {
+    const data = await fetch(
+        `https://api.pexels.com/v1/curated?per_page=10&page=${pagenr}`,
+        {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                Authorization: "563492ad6f91700001000001600ac8a9d73b45a3816e9904cf0654b1"
+            },
+        }
+    )
+    const result = await data.json()
+    result.photos.forEach((photo) => {
+        const pic = document.createElement("div")
+        pic.innerHTML = `<img src=${photo.src.large}        >
+            <p>Photo: ${photo.photographer}</p>
+        <a href= ${photo.src.large}>Enlarge</a>
+        `
+        document.querySelector(".gallery").appendChild(pic)
+    })  
+}
+
+
+async function SearchPhotos(query,pagenr) {
+    const data = await fetch(
+        `https://api.pexels.com/v1/search?query=${query}&per_page=10&page=${pagenr}`,
+        {
+            method: "GET",
+            headers: {
+                Accept: "application/json",
+                Authorization: "563492ad6f91700001000001600ac8a9d73b45a3816e9904cf0654b1"
+            },
+        }
+    )
+    const result = await data.json()
+    result.photos.forEach((photo) => {
+        const pic = document.createElement("div")
+        pic.innerHTML = `<img src=${photo.src.large}        >
+            <p>Photo: ${photo.photographer}</p>
+        <a href= ${photo.src.large}>Enlarge</a>
+        `
+        document.querySelector(".gallery").appendChild(pic)
+    })  
+}
+
+
+searchbutton.addEventListener("click", () => {
+    if (input.value == "") return
+    clear()
+    search = true
+    SearchPhotos(query,pagenr)
+    pagenr ++
+})
+function clear() {
+    input.value =""
+    document.querySelector(".gallery").innerHTML = ""
+    pagenr = 1
+}
+
+
+next.addEventListener("click", ()=> {
+    if (!search){
+        pagenr ++
+        CuratedPhotos(pagenr)
+    }else {
+        if (query.value == ""){
+            return
+        }
+        pagenr++
+        SearchPhotos(query,pagenr)
     }
-}
-
-start()
-
-function createBreedList(breedList) {
-    document.getElementById("breed").innerHTML = `
-    <select onchange = "loadByBreed(this.value)">
-            <option>Choose a dog breed</option> 
-           ${Object.keys(breedList).map(function (breed) {
-                return `<option>${breed}</option>`
-           }).join('')}
-        </select>
-    `
-}
-
-async function loadByBreed(breed) {
-    if (breed != "Choose a dog breed"){
-        const response = await fetch(`https://dog.ceo/api/breed/${breed}/images`)
-        const data = await response.json()
-        createSlideShow(data.message)
-    }
-}
-
-function createSlideShow(images) {
-    let currentPosition = 0
-    clearInterval(timer)
-    clearTimeout(deleteFirstPhotoDelay)
-
-   if (images.length > 1){
-    document.getElementById("slideshow").innerHTML = `
-    <div class="slide" style ="background-image: url('${images[0]}')"></div>
-    <div class="slide" style ="background-image: url('${images[1]}')"></div>
-    `
-    currentPosition += 2
-    if (images.length == 2)currentPosition = 0
-    timer = setInterval(nextSlide,3000)
-   }else{
-    document.getElementById("slideshow").innerHTML = `
-    <div class="slide" style ="background-image: url('${images[0]}')"></div>
-    <div class="slide"></div>
-    ` 
-   }
-
-function nextSlide() {
-    document.getElementById("slideshow").insertAdjacentHTML("beforeend", `<div class="slide" style="background-image: url('${images[currentPosition]}')"></div>`)
-    deleteFirstPhotoDelay = setTimeout(function () {
-        document.querySelector(".slide").remove()
-    }, 1000)
-    if (currentPosition + 1 >= images.length) {
-        currentPosition = 0
-    } else {
-        currentPosition++
-    }
- }
-}
+})
+CuratedPhotos(pagenr)
