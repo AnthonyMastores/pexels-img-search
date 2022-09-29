e4201eb5e6174d821fc8388ab5c892d482346707
Anthony Mastores <mastores27@gmail.com>
2022-09-28 22:49:55 -0500

    cleanup js

--- main.js
+++ main.js
@@ -6,34 +6,6 @@ let pagenr = 1
 let search = false
 let query = ""
 
-input.addEventListener("input",(e) => {
-    e.preventDefault()
-    query = e.target.value
-})
-
-async function CuratedPhotos(pagenr) {
-    const data = await fetch(
-        `https://api.pexels.com/v1/curated?per_page=10&page=${pagenr}`,
-        {
-            method: "GET",
-            headers: {
-                Accept: "application/json",
-                Authorization: "563492ad6f91700001000001600ac8a9d73b45a3816e9904cf0654b1"
-            },
-        }
-    )
-    const result = await data.json()
-    result.photos.forEach((photo) => {
-        const pic = document.createElement("div")
-        pic.innerHTML = `<img src=${photo.src.large}        >
-            <p>Photo: ${photo.photographer}</p>
-        <a href= ${photo.src.large}>Enlarge</a>
-        `
-        document.querySelector(".gallery").appendChild(pic)
-    })  
-}
-
-
 async function SearchPhotos(query,pagenr) {
     const data = await fetch(
         `https://api.pexels.com/v1/search?query=${query}&per_page=10&page=${pagenr}`,
@@ -46,16 +18,26 @@ async function SearchPhotos(query,pagenr) {
         }
     )
     const result = await data.json()
-    result.photos.forEach((photo) => {
+    result.photos.forEach((photo) => { 
         const pic = document.createElement("div")
         pic.innerHTML = `<img src=${photo.src.large}        >
             <p>Photo: ${photo.photographer}</p>
-        <a href= ${photo.src.large}>Enlarge</a>
+        <a href= ${photo.src.large}>Expand  </a>
         `
         document.querySelector(".gallery").appendChild(pic)
-    })  
+    }) 
 }
 
+function clear() {
+    input.value =""
+    document.querySelector(".gallery").innerHTML = ""
+    pagenr = 1
+}
+
+input.addEventListener("input",(e) => {
+    e.preventDefault()
+    query = e.target.value
+})
 
 searchbutton.addEventListener("click", () => {
     if (input.value == "") return
@@ -64,12 +46,6 @@ searchbutton.addEventListener("click", () => {
     SearchPhotos(query,pagenr)
     pagenr ++
 })
-function clear() {
-    input.value =""
-    document.querySelector(".gallery").innerHTML = ""
-    pagenr = 1
-}
-
 
 next.addEventListener("click", ()=> {
     if (!search){
@@ -83,4 +59,4 @@ next.addEventListener("click", ()=> {
         SearchPhotos(query,pagenr)
     }
 })
-CuratedPhotos(pagenr)
+
